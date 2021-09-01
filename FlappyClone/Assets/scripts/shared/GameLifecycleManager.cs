e55c576111c74263ae6cf891ca92dc007ccac65d
Dan Vicarel <shundra8820@gmail.com>
2021-09-01 01:07:05 -0400

    Implemented the gameover buttons
    Required some tweaking to our UI raycasting setup

---
+++ FlappyClone/Assets/scripts/shared/GameLifecycleManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FlappyClone
+{
+    public class GameLifecycleManager : MonoBehaviour
+    {
+        [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "UnityEvents can't call static methods")]
+        public void Restart() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "UnityEvents can't call static methods")]
+        public void Quit() => Application.Quit();
+    }
+}

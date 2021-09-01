eb2b16ef58c0e002b090f725ce6f3a0e65eecd50
Dan Vicarel <shundra8820@gmail.com>
2021-09-01 01:32:32 -0400

    Implemented score incrementing/display

---
+++ FlappyClone/Assets/scripts/shared/Scorekeeper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using TMPro;
+using UnityEngine;
+
+namespace FlappyClone
+{
+    public class Scorekeeper : MonoBehaviour
+    {
+        public const string DefaultFormatString = "Score: {0:##,###}";
+        public const int DefaultInitialScore = 0;
+        public const int DefaultMaxScore = 99_999;
+        public const string DefaultOverMaxString = "Score: 99,999+";
+
+        public int CurrentScore = 0;
+
+        [Space]
+        public TMP_Text ScoreTxt;
+        public string ScoreFormatString = DefaultFormatString;
+        public int InitialScore = DefaultInitialScore;
+        public int MaxScore = DefaultMaxScore;
+        public string OverMaxString = DefaultOverMaxString;
+
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity message")]
+        private void Reset()
+        {
+            ScoreTxt = null;
+            ScoreFormatString = DefaultFormatString;
+            InitialScore = DefaultInitialScore;
+            MaxScore = DefaultMaxScore;
+            OverMaxString = DefaultOverMaxString;
+        }
+
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity message")]
+        private void Awake()
+        {
+            this.AssertAssociation(ScoreTxt, nameof(ScoreTxt));
+        }
+
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity message")]
+        private void Start()
+        {
+            CurrentScore = InitialScore;
+            updateText();
+        }
+
+        public void Increment()
+        {
+            ++CurrentScore;
+            updateText();
+        }
+
+        private void updateText() => ScoreTxt.text = CurrentScore > MaxScore ? OverMaxString : string.Format(ScoreFormatString, CurrentScore);
+    }
+}

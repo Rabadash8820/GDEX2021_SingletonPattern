3dfe5043d90615629fcb014eba61a5ea1de6b26f
Dan Vicarel <shundra8820@gmail.com>
2021-09-11 18:49:27 -0400

    Added the Singleton component from which all static singletons will derive

---
+++ FlappyClone/Assets/scripts/Singletons/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+namespace FlappyClone.Singletons
+{
+    [DisallowMultipleComponent]
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        public static T Instance { get; private set; }
+
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity message")]
+        private void Awake()
+        {
+            Instance = Instance == null
+                ? GetComponent<T>()
+                : throw new Exception($"Singleton type '{typeof(T).Name}' already has an instance at '${string.Join(".", Instance.transform.parent.name, Instance.transform.name)}'");
+
+            OnAwake();
+        }
+
+        protected virtual void OnAwake() { }
+
+    }
+}

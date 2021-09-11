b57cb82ca368feee1d81af823220526fce909682
Dan Vicarel <shundra8820@gmail.com>
2021-09-11 01:20:05 -0400

    Added ObstacleEventListenerTest for demonstration

---
+++ FlappyClone/Assets/Editor/FlappyClone.Tests/ObstacleEventListenerTest.cs
@@ -0,0 +1,65 @@
+using FlappyClone;
+using NUnit.Framework;
+using UnityEngine;
+
+
+public class ObstacleEventListenerTest
+{
+    [Test]
+    public void PlayerClear_IncrementsScore()
+    {
+        // ARRANGE
+        var obj = new GameObject();
+
+        var wallRootObj = new GameObject();
+        WallData wallData = wallRootObj.AddComponent<WallData>();
+        wallRootObj.AddComponent<BoxCollider2D>();
+        wallData.ClearTrigger = wallRootObj.AddComponent<CollisionTrigger2D>();
+
+        var wallTopObj = new GameObject();
+        wallTopObj.transform.parent = wallRootObj.transform;
+        wallTopObj.AddComponent<BoxCollider2D>();
+        wallData.TopCollider = wallTopObj.AddComponent<CollisionTrigger2D>();
+        wallData.TopSprite = wallTopObj.AddComponent<SpriteRenderer>();
+        wallData.TopAnimator = wallTopObj.AddComponent<Animator>();
+
+        var wallBottomObj = new GameObject();
+        wallBottomObj.transform.parent = wallRootObj.transform;
+        wallBottomObj.AddComponent<BoxCollider2D>();
+        wallData.BottomCollider = wallBottomObj.AddComponent<CollisionTrigger2D>();
+        wallData.BottomSprite = wallBottomObj.AddComponent<SpriteRenderer>();
+        wallData.BottomAnimator = wallBottomObj.AddComponent<Animator>();
+
+        ObstacleEventSource obstacleEventSource = obj.AddComponent<ObstacleEventSource>();
+        obstacleEventSource.WallLifecycleManager = obj.AddComponent<WallLifecycleManager>();
+        obstacleEventSource.GroundCollisionTrigger = obj.AddComponent<CollisionTrigger2D>();
+
+        WallLifecycleManager wallLifecycleManager = obj.AddComponent<WallLifecycleManager>();
+        wallLifecycleManager.WallSizer = obj.AddComponent<WallSizer>();
+        wallLifecycleManager.WallPrefab = wallRootObj;
+        wallLifecycleManager.WallParent = obj.transform;
+        wallLifecycleManager.FillBasis = obj.transform;
+
+        WorldMover worldMover = obj.AddComponent<WorldMover>();
+        worldMover.WorldRoot = obj.transform;
+        worldMover.GroundSprite = obj.AddComponent<SpriteRenderer>();
+
+        Scorekeeper scorekeeper = obj.AddComponent<Scorekeeper>();
+
+        HighScoreManager highScoreManager = obj.AddComponent<HighScoreManager>();
+        highScoreManager.Scorekeeper = scorekeeper;
+
+        ObstacleEventListener obstacleEventListener = obj.AddComponent<ObstacleEventListener>();
+        obstacleEventListener.ObstacleEventSource = obstacleEventSource;
+        obstacleEventListener.WallLifecycleManager = wallLifecycleManager;
+        obstacleEventListener.WorldMover = worldMover;
+        obstacleEventListener.Scorekeeper = scorekeeper;
+        obstacleEventListener.HighScoreManager = highScoreManager;
+
+        // ACT
+        obstacleEventListener.HandlePlayerClearedWall(wallData);
+
+        // ASSERT
+        Assert.That(scorekeeper.CurrentScore, Is.EqualTo(scorekeeper.InitialScore + 1));
+    }
+}

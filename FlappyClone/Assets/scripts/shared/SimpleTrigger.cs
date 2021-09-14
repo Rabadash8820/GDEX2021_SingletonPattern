577ccc512e018659e14d9af7a6f8daed053916ff
Dan Vicarel <shundra8820@gmail.com>
2021-09-14 00:55:36 -0400

    All implementations now use common UnityEvents for clear/collision events

---
+++ FlappyClone/Assets/scripts/shared/SimpleTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace FlappyClone.Shared
+{
+    public class SimpleTrigger : MonoBehaviour
+    {
+        public UnityEvent Triggered = new UnityEvent();
+        public void Trigger() => Triggered.Invoke();
+    }
+}
